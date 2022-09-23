9144878f84101b359642ed79b26cd1d63fd19745
Alvaro Alvaro <alvsr89@gmail.com>
2022-09-23 14:42:57 +0200

    first commit

---
+++ src/Ejercicio3/ClaseParte1.java
@@ -0,0 +1,20 @@
+package Ejercicio3;
+
+public class ClaseParte1 {
+
+    public static void main(String[] args) {
+        int resultado;
+        resultado=sumatorio(1,2,3);
+
+        System.out.println(resultado);
+        
+
+    }
+
+    public static int sumatorio(int a, int b, int c) {
+        return a+b+c;
+
+    }
+
+
+}
